1de05a01c0a7bb247d6186dad91ec5b717849f0a
sharon gamboa <sharongamboa01@hotmail.com>
2023-01-22 19:33:21 -0400

    table projects - test get projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
+
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
