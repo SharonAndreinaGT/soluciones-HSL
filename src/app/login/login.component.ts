4eb67d4695eae31fe70f45c7a7c240efdd3e0cc1
henrry bourgeot <henrrybrgt@gmail.com>
2023-01-28 17:39:35 -0400

    mejorado login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,9 @@ import { UserService } from '../services/user.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-
-export class LoginComponent  {
+export class LoginComponent {
   hide = true;
   username: string;
   password: string;
@@ -18,15 +17,15 @@ export class LoginComponent  {
   constructor(
     private _router: Router,
     private http: HttpClient,
-    private userService: UserService) { }
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
     this.userService.clearData();
   }
 
   login() {
-    if (this.username!= undefined && this.password!= undefined) {
-
+    if (this.username != undefined && this.password != undefined) {
       // this.http.get('http://127.0.0.1:8080/login/'+this.username+'/'+this.password).subscribe((data) => {
       //   if (data.confirmado == 'si') {
       //     this.userService.storeUserData('username', this.username);
@@ -38,17 +37,20 @@ export class LoginComponent  {
       //   }
       // })
 
-      this.userService.storeUserData('username', this.username);
-      this.userService.storeUserData('isAdmin', 'si');
-      this.errorValidateLogin = false;
-      this._router.navigate(['projects']);
-
+      this.http
+        .get(`http://localhost:8000/login/${this.username}/${this.password}`)
+        .subscribe((data: any) => {
+          if (data.confirmado === 'si') {
+            this.userService.storeUserData('username', this.username);
+            this.userService.storeUserData('isAdmin', data.administrador);
+            this.errorValidateLogin = false;
+            this._router.navigate(['projects']);
+          } else {
+            this.errorValidateLogin = true;
+          }
+        });
     } else {
       this.errorValidateLogin = true;
     }
-
-
   }
 }
-
-
